045b16fc8cf73b94892bc90373f60b71a648ce6b
amoblin <amoblin@gmail.com>
2013-05-13 14:24:24 +0800

    jsonp

--- js/google-reader-web.js
+++ js/google-reader-web.js
@@ -68,13 +68,13 @@ validateToken = function(token) {
 
 getSubscription = function() {
   return $.getJSON({
-    url: 'https://www.google.com/reader/api/0/subscription/list?output=json',
+    url: 'https://www.google.com/reader/api/0/subscription/list?output=json' + "&callback=?",
     dataType: "jsonp",
     success: function(data) {
         console.log(data);
-    console.log("******");
-    console.log(data);
-    return importFromGoogleReader(JSON.parse(data).subscriptions);
+        console.log("******");
+        console.log(data);
+        return importFromGoogleReader(JSON.parse(data).subscriptions);
     }
   });
 };
